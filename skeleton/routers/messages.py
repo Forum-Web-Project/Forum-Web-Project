5d0302ff7b1f9007b27bf65dc4e53c303b07f702
Georgi Dodekov <g.dodekov@gmail.com>
2023-10-30 22:24:40 +0200

    Updated message functionality

--- skeleton/routers/messages.py
+++ skeleton/routers/messages.py
@@ -1,29 +1,39 @@
 from fastapi import APIRouter, Header, HTTPException
+from pydantic import BaseModel
 from common.auth import get_user_or_raise_401
-from services import message_service
+from services import message_service, user_service
 from data.models import Message, User
 from common.responses import NotFound, Unauthorized
-from services.message_service import check_receiver_name
+from services.message_service import check_receiver_name, conversation_by_username
 from services.user_service import find_by_username, check_username_exist
+
 
 message_router = APIRouter(prefix='/message')
 
 
-@message_router.get('/', response_model=list[Message])
-def get_conversations(token: str = Header()):
-    user = get_user_or_raise_401(token)
-
+@message_router.get('/{id}', response_model=list[Message])
+def get_conversations(id: int, x_token: str = Header()):
+    user = get_user_or_raise_401(x_token)
     if not user:
-        return Unauthorized('Not authenticated user!')
+        return NotFound()
 
-    messages = message_service.all()
+    messages = message_service.all_user_related_message(id)
 
     return messages
 
 
-@message_router.get('/{username}')
-def get_conversation_by_username():
-    pass
+@message_router.get('/{name}', tags=["Receiver username"], response_model=list[Message])
+def get_conversation_by_username(name: str, x_token: str = Header()):
+    user = get_user_or_raise_401(x_token)
+    if not user:
+        return NotFound("Not authenticated user!")
+
+    result = conversation_by_username(name)
+
+    if not result:
+        return NotFound('No message found!')
+
+    return result
 
 
 @message_router.post('/', tags=["Create message"])
@@ -38,7 +48,5 @@ def create_message(message: Message, x_token: str = Header()):
         return created_message
 
     return NotFound('Receiver name not exist!')
-
-
 
 
--- skeleton/services/message_service.py
+++ skeleton/services/message_service.py
@@ -14,6 +14,64 @@ def all():
 def get_conversation_by_user_id():
     pass
 
+from data.database import read_query, insert_query
+from data.models import Message
+from services import user_service
+
+
+def all_user_related_message(id: int):
+    # data = read_query(
+    #     '''SELECT id, text, sender_id, receiver_username FROM messages WHERE sender_id = ?''',
+    #     (id,))
+    #
+    # if not data:
+    #     return None
+    #
+    # return (Message.from_query_result(*row) for row in data)
+
+    # The method above return id, text, sender_id, receiver_username
+
+    data = read_query(
+        '''SELECT receiver_username FROM messages WHERE sender_id = ?''',
+        (id,))
+
+    if not data:
+        return None
+
+    return next((Message(receiver_username=receiver_username) for receiver_username in data), None)
+
+
+def conversation_by_username(name: str = None):
+
+    data = read_query(
+        '''SELECT id, text, sender_id, receiver_username
+           FROM messages 
+           WHERE receiver_username LIKE ?''', (name,))
+
+    return (Message.from_query_result(*row) for row in data)
+
+
+def create(message: Message) -> Message | None:
+
+    generated_id = insert_query(
+        '''INSERT INTO messages(id, text, sender_id, receiver_username) VALUES (?,?,?,?)''',
+        (message.id, message.text, message.sender_id, message.receiver_username))
+
+    message.id = generated_id
+
+    return message
+
+
+def check_receiver_name(receiver_username: str) -> bool:
+    data = read_query(
+        '''SELECT username FROM users WHERE username = ?''',
+        (receiver_username,)
+    )
+
+    result = data
+
+    return bool(result)
+
 
 def create(message: Message) -> Message | None:
 
