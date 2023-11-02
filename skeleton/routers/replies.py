cc24fb24aaa67fb7ec1841b8d7b37a24c5590e48
easymneysniper <mariotelerikacademy@gmail.com>
2023-11-02 14:53:43 +0200

    Upvote/Downvote reply - complete and tested.

--- skeleton/routers/replies.py
+++ skeleton/routers/replies.py
@@ -1,8 +1,29 @@
 from fastapi import APIRouter, Response, Header, HTTPException, Query
 from services import user_service, topic_service, category_service, reply_service
 from common.auth import get_user_or_raise_401
+from fastapi.responses import JSONResponse
 
 reply_router = APIRouter(prefix='/reply', tags=['Replies'])
+
+_SEPARATOR = ';'
+
+@reply_router.get('/', description='Get All Replies')
+def get_replies():
+    replies = reply_service.read_replies()
+
+    result = []
+    for data in replies:
+        reply_dict = {
+            "id": data[0],
+            "text": data[1],
+            "users_id": data[2],
+            "topics_id": data[3],
+            "upvotes": data[4],
+            "downvotes": data[5]
+        }
+        result.append(reply_dict)
+    return result
+
 
 @reply_router.post("/create_reply")
 def create_reply(x_token: str = Header(),
@@ -17,3 +38,59 @@ def create_reply(x_token: str = Header(),
     else:
         result = reply_service.create_reply(text, username, topic_name)
         return result
+
+
+@reply_router.put("/upvote_reply")
+def upvote_reply(x_token: str = Header(),
+                 reply_id: int = Query()
+                 ):
+    user = get_user_or_raise_401(x_token)
+    user_id, _ = x_token.split(_SEPARATOR)
+
+    if reply_service.check_if_already_upvoted(user_id, reply_id):
+        return JSONResponse(status_code=400, content=f'You already upvoted this reply!')
+    
+    if reply_service.check_if_already_downvoted(user_id, reply_id):
+        # reply_service.remove_downvote_from_reply(reply_id)
+        # reply_service.add_upvote_to_reactions(user_id, reply_id)
+        # result = reply_service.upvote_reply(reply_id)
+        # return result
+        reply_service.remove_downvote_from_reply(reply_id)
+        reply_service.set_reaction_to_one(user_id, reply_id)
+        reply_service.upvote_reply(reply_id)
+        return JSONResponse(status_code=200, content=f'Upvote complete!')
+
+    if not reply_service.check_reply_exists_by_id(reply_id):
+        return JSONResponse(status_code=400, content=f'Wrong ID, no such reply!')
+    else:
+        reply_service.upvote_reply(reply_id)
+        reply_service.add_upvote_to_reactions(user_id, reply_id)
+        return JSONResponse(status_code=200, content=f'Upvote complete!')
+    
+
+
+@reply_router.put("/downvote_reply")
+def downvote_reply(x_token: str = Header(),
+                 reply_id: int = Query()
+                 ):
+    user = get_user_or_raise_401(x_token)
+    user_id, _ = x_token.split(_SEPARATOR)
+
+    if reply_service.check_if_already_downvoted(user_id, reply_id):
+        return JSONResponse(status_code=400, content=f'You already downvoted this reply!')
+    
+    if reply_service.check_if_already_upvoted(user_id, reply_id):
+        # reply_service.remove_upvote_from_reply(reply_id)
+        # reply_service.add_downvote_to_reactions(user_id, reply_id)
+        reply_service.remove_upvote_from_reply(reply_id)
+        reply_service.set_reaction_to_zero(user_id, reply_id)
+        reply_service.downvote_reply(reply_id)
+        return JSONResponse(status_code=200, content=f'Downvote complete!')
+
+
+    if not reply_service.check_reply_exists_by_id(reply_id):
+        return JSONResponse(status_code=400, content=f'Wrong ID, no such reply!')
+    else:
+        reply_service.downvote_reply(reply_id)
+        reply_service.add_downvote_to_reactions(user_id, reply_id)
+        return JSONResponse(status_code=200, content=f'Downvote complete!')
--- skeleton/services/reply_service.py
+++ skeleton/services/reply_service.py
@@ -1,4 +1,4 @@
-from data.database import read_query,insert_query
+from data.database import read_query,insert_query,update_query
 from data.models import Reply
 from services import topic_service
 from fastapi import Response
@@ -43,3 +43,85 @@ def get_reply_by_topic_id(topic_id: int):
      )
      replies = [{'text': row[1], 'users_id': row[2], 'topics_id': row[3], 'upvotes': row[4], 'downvotes': row[5]} for row in data]
      return replies
+
+def read_replies():
+    data = read_query('SELECT * FROM replies')
+    return data
+
+def check_if_already_upvoted(users_id, reply_id):
+     data = read_query(
+          "SELECT users_id, replies_id FROM reactions WHERE users_id = ? AND replies_id = ? and reaction = ?",
+          (users_id, reply_id, 1)
+     )
+     return bool(data)
+
+def check_if_already_downvoted(users_id, reply_id):
+     data = read_query(
+          "SELECT users_id, replies_id FROM reactions WHERE users_id = ? AND replies_id = ? and reaction = ?",
+          (users_id, reply_id, 0)
+     )
+     return bool(data)
+
+def remove_downvote_from_reply(reply_id: int):
+     data = update_query(
+          "UPDATE replies SET downvotes = downvotes - 1 WHERE id = ?",
+          (reply_id,)
+     )
+     return data
+
+def set_reaction_to_one(users_id, reply_id):
+     data = update_query(
+          "UPDATE reactions SET reaction = 1 WHERE users_id = ? and replies_id = ? and reaction = ?",
+          (users_id, reply_id, 0)
+     )
+     return data
+
+def upvote_reply(reply_id: int):
+     data = update_query(
+          "UPDATE replies SET upvotes = upvotes + 1 WHERE id = ?",
+          (reply_id,)
+     )
+     return data
+
+def check_reply_exists_by_id(id: int) -> bool:
+
+        data = read_query(
+            'SELECT id FROM replies WHERE id = ?',
+            (id,)
+        )
+        return bool(data)
+
+def add_upvote_to_reactions(user_id, reply_id):
+    data = insert_query(
+         "INSERT INTO reactions(users_id, replies_id, reaction) VALUES(?,?,?)",
+         (user_id, reply_id, 1,)
+    )
+    return data
+
+def remove_upvote_from_reply(reply_id: int):
+     data = update_query(
+          "UPDATE replies SET upvotes = upvotes - 1 WHERE id = ?",
+          (reply_id,)
+     )
+     return data
+
+def set_reaction_to_zero(users_id, reply_id):
+     data = update_query(
+          "UPDATE reactions SET reaction = 0 WHERE users_id = ? and replies_id = ? and reaction = ?",
+          (users_id, reply_id, 1)
+     )
+     return data
+
+def downvote_reply(reply_id: int):
+     data = update_query(
+          "UPDATE replies SET downvotes = downvotes + 1 WHERE id = ?",
+          (reply_id,)
+     )
+     return data
+
+def add_downvote_to_reactions(user_id, reply_id):
+    data = insert_query(
+         "INSERT INTO reactions(users_id, replies_id, reaction) VALUES(?,?,?)",
+         (user_id, reply_id, 0,)
+    )
+    return data
